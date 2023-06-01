5f9b9861ccf216b27b237dbb795cb32dc74bf16a
mac13 <mac13@mac13.local>
2023-06-01 14:09:51 +0300

    first commit on server

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 8080;
+const path = __dirname + '/build/';
+app.use(express_1.default.static(path));
+app.get('/', function (req, res) {
+    res.sendFile(path + 'index.html');
+});
+app.get('/test', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const data = { foo: 'bar' };
+    res.json(data);
+});
+app.listen(PORT, () => {
+    console.log(`Server running at ${PORT}`);
+});
---
+++ index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+export interface QueryPayload {
+  foo: string;
+}
+
+const path = __dirname + '/build/';
+
+app.use(express.static(path));
+
+app.get('/', function (req, res) {
+  res.sendFile(path + 'index.html');
+});
+
+app.get('/test', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  const data: QueryPayload = { foo: 'bar' };
+  res.json(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
+});
